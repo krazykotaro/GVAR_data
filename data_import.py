97ae5e3ea55685fa8afb06de2d9d58962911fb90
EC2 Default User <ec2-user@ip-172-31-21-39.ap-northeast-1.compute.internal>
2017-10-06 12:41:35 +0000

    data importプログラム

---
+++ data_import.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+
+import pandas as pd
+import requests
+
+#USのGDPを取得
+url = 'http://dataservices.imf.org/REST/SDMX_JSON.svc/CompactData/IFS/Q.US.NGDP_R_CH_SA_XDC?startPeriod=1957&endPeriod=2016'
+
+# Get data from the above URL using the requests package
+data = requests.get(url).json()
+
+# Load data into a pandas dataframe
+base_year = pd.DataFrame(data['CompactData']['DataSet']['Series'])["@BASE_YEAR"]
+country = pd.DataFrame(data['CompactData']['DataSet']['Series'])["@REF_AREA"]
+obs = pd.DataFrame(data['CompactData']['DataSet']['Series']['Obs'])
+# データを連結
+data = pd.concat([base_year, country, obs], axis = 1)
+
+#データを整理
+data.columns = ["base_year", "country", "rgdp", "time"]
+data = data.ix[:, ["time", "country", "rgdp", "base_year"]]
+
+#test
+data.to_csv("test.csv")
